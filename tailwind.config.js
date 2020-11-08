86e274f6e44cd0b6331b9a1c35bcc5e7d472a0bd
ebxn <ebengtuff@gmail.com>
2020-11-08 14:49:17 +0000

    remove vue-cli boilerplate, configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
+const dT = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+  future: 'all',
+  experimental: 'all',
+  dark: 'class',
+  purge: {
+    content: [
+      'public/index.html',
+      'src/**/*.vue'
+    ],
+    options: {
+      keyframes: true
+    }
   },
-  purge: [],
   theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...dT.fontFamily.sans],
+        handwrite: ['Comic Neue', 'cursive']
+      }
+    }
+  }
 }
